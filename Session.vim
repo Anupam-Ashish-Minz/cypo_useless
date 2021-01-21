66b7a8303123136703423b0fdd91e986fe6fbf07
Anupam Ashish Minz <anupamminz300am@gmail.com>
2021-01-21 23:44:52 +0530

    Upload first think later .. maybe inital commit

---
+++ Session.vim
@@ -0,0 +1,67 @@
+let SessionLoad = 1
+let s:so_save = &g:so | let s:siso_save = &g:siso | setg so=0 siso=0 | setl so=-1 siso=-1
+let v:this_session=expand("<sfile>:p")
+silent only
+silent tabonly
+cd ~/work/project/cypo
+if expand('%') == '' && !&modified && line('$') <= 1 && getline(1) == ''
+  let s:wipebuf = bufnr('%')
+endif
+set shortmess=aoO
+badd +4 src/App.tsx
+badd +19 src/CurrentList.tsx
+badd +2 src/CurrencyList.tsx
+argglobal
+%argdel
+$argadd ./
+edit src/App.tsx
+set splitbelow splitright
+set nosplitright
+wincmd t
+set winminheight=0
+set winheight=1
+set winminwidth=0
+set winwidth=1
+argglobal
+setlocal fdm=manual
+setlocal fde=0
+setlocal fmr={{{,}}}
+setlocal fdi=#
+setlocal fdl=0
+setlocal fml=1
+setlocal fdn=5
+setlocal fen
+silent! normal! zE
+9,63fold
+65,78fold
+79,101fold
+102,108fold
+109,114fold
+115,129fold
+138,150fold
+153,163fold
+130,169fold
+let s:l = 1 - ((0 * winheight(0) + 21) / 42)
+if s:l < 1 | let s:l = 1 | endif
+exe s:l
+normal! zt
+1
+normal! 019|
+lcd ~/work/project/cypo/src
+tabnext 1
+if exists('s:wipebuf') && len(win_findbuf(s:wipebuf)) == 0&& getbufvar(s:wipebuf, '&buftype') isnot# 'terminal'
+  silent exe 'bwipe ' . s:wipebuf
+endif
+unlet! s:wipebuf
+set winheight=1 winwidth=20 winminheight=1 winminwidth=1 shortmess=filnxtToOFA
+let s:sx = expand("<sfile>:p:r")."x.vim"
+if filereadable(s:sx)
+  exe "source " . fnameescape(s:sx)
+endif
+let &g:so = s:so_save | let &g:siso = s:siso_save
+nohlsearch
+let g:this_session = v:this_session
+let g:this_obsession = v:this_session
+doautoall SessionLoadPost
+unlet SessionLoad
+" vim: set ft=vim :
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import './App.css';
 import { useState, useEffect } from 'react';
 import Fuse from 'fuse.js';
+import { TextField, InputAdornment, Container, Grid, Accordion,
+    AccordionSummary, AccordionDetails, Typography,
+} from '@material-ui/core';
+import { Search as SearchIcon } from '@material-ui/icons';
 
-interface CoinGeckoApi {
+export interface CoinGeckoApi {
     //id	"bitcoin"
     //symbol	"btc"
     //name	"Bitcoin"
@@ -85,9 +89,13 @@ function App() {
                 score: number
             };
             const fuse = new Fuse(data, fuseOptions);
-            const searchResult: FuseResult[] = fuse.search(searchValue) as FuseResult[];
-            const result: CoinGeckoApi[] = searchResult.map((x: FuseResult) => x.item);
-            setDisplayData(result);
+            if (searchValue) {
+                const searchResult: FuseResult[] = fuse.search(searchValue) as FuseResult[];
+                const result: CoinGeckoApi[] = searchResult.map((x: FuseResult) => x.item);
+                setDisplayData(result);
+            } else {
+                setDisplayData(data);
+            }
         }
     };
 
@@ -120,47 +128,42 @@ function App() {
     }, [searchValue]);
 
     return (
-        <>
-            <input type="text" className="search-box" onChange={handleInput} />
-            {console.log(displayData)}
-            {displayData ? displayData.map((elem: any) => {
-                return (
-                    /*
-                    id	"bitcoin"
-                    symbol	"btc"
-                    name	"Bitcoin"
-                    image	"https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579"
-                    current_price	35130
-                    market_cap	653611312633
-                    market_cap_rank	1
-                    fully_diluted_valuation	737738793910
-                    total_volume	50198011517
-                    high_24h	37690
-                    low_24h	35021
-                    price_change_24h	-1422.00571619
-                    price_change_percentage_24h	-3.89032
-                    market_cap_change_24h	-26813280208.433838
-                    market_cap_change_percentage_24h	-3.94067
-                    circulating_supply	18605281
-                    total_supply	21000000
-                    max_supply	21000000
-                    ath	41940
-                    ath_change_percentage	-16.23667
-                    ath_date	"2021-01-08T15:05:37.863Z"
-                    atl	67.81
-                    atl_change_percentage	51707.89977
-                    atl_date	"2013-07-06T00:00:00.000Z"
-                    roi	null
-                    last_updated	"2021-01-20T04:19:36.500Z"
-                    */
-                    <div key={elem.id}>
-                        <img src={elem.image} alt="not found" className="api-image" />
-                        <div className="api-name">{elem.name}</div>
-                        <div className="api-current_price">usd {elem.current_price}</div>
-                    </div>
-                );
-            }) : null}
-        </>
+        <Container style={{marginTop: "10px"}}>
+            <Grid
+                container
+                direction="column"
+                justify="center"
+                alignItems="stretch" >
+                { /*<input type="text" className="search-box" onChange={handleInput} /> */}
+                <Grid item>
+                    <TextField 
+                        variant="outlined" 
+                        onChange={handleInput} 
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    <SearchIcon />
+                                </InputAdornment>
+                            )
+                        }}
+                    />
+                </Grid>
+                {console.log(displayData)}
+                {displayData ? displayData.map((elem: any) => {
+                    return (
+                        <Accordion key={elem.id} >
+                            <AccordionSummary>
+                                <img src={elem.image} alt="not found" className="api-image" style={{marginRight:"10px"}} />
+                                <Typography variant="h6">{elem.name}</Typography>
+                            </AccordionSummary>
+                            <AccordionDetails>
+                                Current Price: ${elem.current_price}
+                            </AccordionDetails>
+                        </Accordion>
+                    );
+                }) : null}
+            </Grid>
+        </Container>
     );
 }
 
